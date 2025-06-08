6eb1450ab3897f1995ca45e36a9fb9a32e92b9c3
Madhav-Sarma <a.madhavsarma@gmail.com>
2025-06-08 10:30:50 +0530

    cron.js aded

--- src/server.js
+++ src/server.js
@@ -5,12 +5,15 @@ import { connectToDatabase } from './config/db.js'; // Import the database conne
 import rateLimieter from './middleware/rateLimiter.js';
 
 import transactionsRoute from './routes/transactionsRoute.js';
+import job from './config/cron.js'; // Import the cron job
 // Load environment variables from .env file
 // Note: Ensure you have a .env file in the root directory with the PORT variable defined
 dotenv.config();
 
 const app = express();
 
+
+if (process.env.NODE_ENV === 'production') job.start(); // Start the cron job if in production environment
 
 //Middleware
 app.use(rateLimieter); // Apply rate limiting middleware
@@ -23,7 +26,9 @@ app.use("/api/transactions",transactionsRoute);
 
 const PORT=process.env.PORT|| 3000;
 
-
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ message: "Server is healthy" });
+});
 
 connectToDatabase().then(() => {
     app.listen(PORT, () => {
